a73942ef123758dc9f141f3ee4823216e9c8a9f0
Bogdan <mormul96@ukr.net>
2020-03-25 14:43:19 +0200

    finish

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,33 @@
 "use strict";
+
+class StringBuilder {
+  constructor(value) {
+    this.value = value;
+  }
+
+  append(str) {
+    this.value += str;
+  }
+
+  prepend(str) {
+    this.value = str + this.value;
+  }
+
+  pad(str) {
+    this.value += str;
+    this.value = str + this.value;
+  }
+}
+
+const builder = new StringBuilder(".");
+
+console.log(builder.value);
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
